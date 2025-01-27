2b4863a34ae21087a3dffbd2f3f3ca73af9d7aa3
Chris Woods <chris@bitspace.org>
2025-01-27 10:08:46 -0500

    markdown mode

--- init.el
+++ init.el
@@ -106,3 +106,13 @@
 
 ;; xscheme for scheme evaluation operations
 (require 'xscheme)
+
+;; markdown-mode config
+(autoload 'markdown-mode "markdown-mode"
+  "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist
+             '("\\.\\(?:md\\|markdown\\|mkd\\|mdown\\|mkdn\\|mdwn\\)\\'" . markdown-mode))
+
+(autoload 'gfm-mode "markdown-mode"
+  "Major mode for editing Github Flavored Markdown files" t)
+(add-to-list 'auto-mode-alist'("README\\.md\\'" . gfm-mode))
