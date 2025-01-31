d686030c8dea64d4cebf2de099bacf844b046dd4
Chris Woods <chris@bitspace.org>
2025-01-31 14:11:53 -0500

    starting to merge in changes from macos

---
+++ stage-for-merge/lisppets.el
@@ -0,0 +1,27 @@
+;; convert a markdown style link to an org style link
+(defun cjw/convert-markdown-link-to-org ()
+  "Convert a markdown style hyperlink to an org-mode style hyperlink in the current line."
+  (interactive)
+  (save-excursion
+    (beginning-of-line)
+    (while (re-search-forward "\\[\\(.*?\\)\\](\\(https://[^)]+\\))" (line-end-position) t)
+      (replace-match "[[\\2][\\1]]"))))
+
+;; compress repeated blank lines into one
+(defun cjw/compress-blank-lines ()
+  "Replace multiple blank lines with a single blank line throughout the buffer."
+  (interactive)
+  (save-excursion
+    (goto-char (point-min))
+    ;; look for two or more consecutive newline characters
+    (while (re-search-forward "\\(\n[ \t]*\\)\n+" nil t)
+      ;; replace them with a single newline
+      (replace-match "\\1"))))
+
+;; Wrapper for visual-line-mode. Additional file types can be added here.
+(defun cjw/enable-visual-line-mode-on-hooks (hooks)
+  "Enable visual-line-mode for each mode specified in HOOKS."
+  (dolist (hook hooks)
+    (add-hook hook 'visual-line-mode)))
+
+(provide 'lisppets)
