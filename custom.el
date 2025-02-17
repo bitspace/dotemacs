ad8c2c8281122afee58a1dc7109e2ccd676bc36e
Chris Woods <chris@bitspace.org>
2025-02-17 04:12:12 -0500

    variabe width fonts for org

--- custom.el
+++ custom.el
@@ -4,10 +4,11 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(ripgrep which-key projectile org-contrib markdown-mode magit ligature geiser-mit catppuccin-theme)))
+   '(org-bullets ripgrep which-key projectile org-contrib markdown-mode magit ligature geiser-mit catppuccin-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(fixed-pitch ((t (:family "JetBrainsMono Nerd Font Mono" :height 140))))
+ '(variable-pitch ((t (:family "Inter" :height 140 :weight medium)))))
